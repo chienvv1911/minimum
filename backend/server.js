04a5d265bca6f270a51c818745a3d71bb225f96e
chienvu94 <chienvv@massgenie.com>
2020-12-17 18:03:02 +0700

    4. Install morgan

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
 
 const app = express()
 dotenv.config()
@@ -10,6 +11,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 app.use(express.json())
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
     res.send('Hello world')
